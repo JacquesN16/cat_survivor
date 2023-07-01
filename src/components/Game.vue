68fd47bd468cc150cb7a3f60feb29d2a43edcef0
JacquesN <jacques@cadeaudelamaison.com>
2023-07-01 07:15:25 +0200

    create canvas with phaser

---
+++ src/components/Game.vue
@@ -0,0 +1,52 @@
+<template>
+  <div ref="gameContainer"></div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+import Phaser from 'phaser';
+
+export default {
+  setup() {
+    const gameContainer = ref(null);
+    let game = null;
+
+    onMounted(() => {
+      game = new Phaser.Game({
+        width: '100%',
+        height: '100%',
+        type: Phaser.AUTO,
+        parent: gameContainer.value,
+        scene: {
+          init() {
+            this.cameras.main.setBackgroundColor('#24252A');
+          },
+          create() {
+            this.helloWorld = this.add.text(
+                this.cameras.main.centerX,
+                this.cameras.main.centerY,
+                'Hello World',
+                { font: '40px Arial', fill: '#ffffff' }
+            );
+            this.helloWorld.setOrigin(0.5);
+          },
+          update() {
+            this.helloWorld.angle += 1;
+          },
+        },
+      });
+    });
+
+    return {
+      gameContainer,
+    };
+  },
+};
+</script>
+
+<style>
+#gameContainer {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
+import {defineCustomElements as defineIonPhaser} from "@ion-phaser/core";
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+
+defineIonPhaser(window);
 
 createApp(App).mount('#app')
